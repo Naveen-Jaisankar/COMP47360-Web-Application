8638b6c095837b8941004fe8122bc33e1aac92e5
jmacap <156478354+jmacap@users.noreply.github.com>
2024-06-06 19:50:23 +0100

    Feature dev: fully functioning Map added into map page. Broken zoom initially but disabling default UI and specifying defaultZoom and defaultCenter seemed to fix this. Not fully investigated though.

--- FrontEnd/air-quality-app/src/pages/Map.jsx
+++ FrontEnd/air-quality-app/src/pages/Map.jsx
@@ -1,10 +1,32 @@
+import {Map,
+        APIProvider,
+        AdvancedMarker,
+        Pin,
+        InfoWindow,
+} from "@vis.gl/react-google-maps"
 
+// resources used:
+// https://www.youtube.com/watch?v=PfZ4oLftItk&list=PL2rFahu9sLJ2QuJaKKYDaJp0YqjFCDCtN
+// https://blog.victorwilliams.me/how-to-add-google-maps-to-react-app-visgl
+const googleMapsKey= import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+const position = { lat: 40.7831, lng: -73.9712 };
 
-
-export default function Map () {
+export default function MapPage () {
     return (
         <>
-        <h1>Here is the Map Page!</h1>
+        <APIProvider apiKey={googleMapsKey}>
+        <div>
+            <Map
+            style={{width: '100vw', height: '100vh'}}
+            defaultZoom={12}
+            defaultCenter={position}
+            gestureHandling={'greedy'}
+            disableDefaultUI={true}></Map>
+
+        </div>
+        
+        
+        </APIProvider>
 
         </>
     )
