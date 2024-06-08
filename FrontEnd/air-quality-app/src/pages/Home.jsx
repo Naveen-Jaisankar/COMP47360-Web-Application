bc45676468dc65b9dd70a9a5bef9aa7d274ff8b9
jmacap <156478354+jmacap@users.noreply.github.com>
2024-06-08 14:15:43 +0100

    Basic semantic homepage no stylings

--- FrontEnd/air-quality-app/src/pages/Home.jsx
+++ FrontEnd/air-quality-app/src/pages/Home.jsx
@@ -1,8 +1,29 @@
+import {Container, Typography} from "@mui/material";
+import {Helmet} from 'react-helmet';
+
 const Home = () =>{
     return(
-        <div>
-            <h1>Welcome to Home Page</h1>
-        </div>
+       <>
+       <Helmet>
+
+       </Helmet>
+
+       {/* Introduction Section */}
+       <section>
+       <Container className="hero-component">
+        <Typography variant="h1" component='h1' sx={{fontSize: '4rem'}}>Introduction to Fair</Typography>
+        <Typography variant="p" component='a'>3-4 lines about our app</Typography>
+       </Container>
+       </section>
+    
+        {/* Info-card Section */}
+       <section></section>
+
+       {/* Tutorial section */}
+       <section></section>
+
+
+       </>
     )
 }
 
