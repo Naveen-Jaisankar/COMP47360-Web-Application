25b9ddbb5f152cb6615afd46c774092ed136f41d
RuthDibenzi <dibenziruth1@gmail.com>
2024-06-13 14:07:02 +0100

    Cross origin resource sharing policy configured to allow front end receive data from backend

--- Backend/src/main/java/com/compsci/webapp/WebappApplication.java
+++ Backend/src/main/java/com/compsci/webapp/WebappApplication.java
@@ -3,7 +3,7 @@ package com.compsci.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.compsci.webapp.exception.CustomLogger;
+
 
 @SpringBootApplication
 public class WebappApplication {
