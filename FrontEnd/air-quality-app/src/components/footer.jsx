abcd435328ccd02b9393d65619a17d7a979869a6
jmacap <156478354+jmacap@users.noreply.github.com>
2024-06-17 11:56:38 +0100

    amended footer with the config file

--- FrontEnd/air-quality-app/src/components/footer.jsx
+++ FrontEnd/air-quality-app/src/components/footer.jsx
@@ -1,3 +1,4 @@
+import contactInfo from './footerconfig';
 import React from 'react';
 
 const Footer = ()=>{
@@ -6,23 +7,23 @@ const Footer = ()=>{
             <div className='flex flex-col overflow-x-hidden sm:flex-row sm:gap-60'>
                 <div className='flex flex-col gap-2 text-center px-2 py-2'>
                     <h3 className='font-bold'>Contact Us</h3>
-                    <p className="text-gray-600 font-semibold">Dublin, Ireland</p>
+                    <p className="text-gray-600 font-semibold">{contactInfo.address}</p>
                     <ul className='flex flex-col gap-2'>
                         <li>
                             <div className='items-center justify-center'>
-                                <p>email@tmail.com</p>
+                                <p>{contactInfo.email}</p>
                             </div>
                         </li>
                         <li>
                             <div className='items-center justify-center'>
-                                <p>+353 011 1018290</p>
+                                <p>{contactInfo.phone}</p>
                             </div>
                         </li>
                     </ul>
                 </div>
                 <div className='flex flex-col items-center gap-2 px-2 py-2'>
-                    <h3 className='font-bold'>Find Us</h3>
-                    <a href="https://maps.app.goo.gl/bgcjih7Xw9YZArhK9">Google maps link</a>
+                    <h3 className='font-bold'>Find us</h3>
+                    <a href={ contactInfo.mapLink }>Google maps link</a>
                 </div>
             </div>
         </footer>
