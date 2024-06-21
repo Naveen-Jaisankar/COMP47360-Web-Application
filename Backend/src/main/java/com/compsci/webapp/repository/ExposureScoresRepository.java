adfd1debef6fd9b1b89782d797b7e032f215ca1a
RuthDibenzi <dibenziruth1@gmail.com>
2024-06-21 16:33:03 +0100

    remove unecessary files

--- Backend/src/main/java/com/compsci/webapp/repository/ExposureScoresRepository.java
+++
@@ -1,9 +0,0 @@
-package com.compsci.webapp.repository;
-
-
-import org.springframework.data.repository.CrudRepository;
-import com.compsci.webapp.model.ExposureScores;
-
-public interface ExposureScoresRepository extends CrudRepository<ExposureScores, Long> {
-    // no methods needed
-}
--- Backend/src/main/java/com/compsci/webapp/repository/StGeorgesQuizRepository.java
+++
@@ -1,8 +0,0 @@
-package com.compsci.webapp.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.compsci.webapp.model.StGeorgesQuiz;
-
-public interface StGeorgesQuizRepository extends CrudRepository<StGeorgesQuiz, Integer> {
-    
-}
