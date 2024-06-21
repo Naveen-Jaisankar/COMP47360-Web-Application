54d89ff598da1b88f1b0984cd874f1d1d339f2e5
jmacap <156478354+jmacap@users.noreply.github.com>
2024-06-21 09:50:23 +0100

    Privacy: final stylings applied, all that will be the content

--- FrontEnd/air-quality-app/src/pages/Privacy.jsx
+++ FrontEnd/air-quality-app/src/pages/Privacy.jsx
@@ -21,7 +21,9 @@ export default function Privacy () {
                     marginBottom: "3rem",
                 }}>
                 <Typography variant="h2" component='h1'>Privacy</Typography>
-                <Typography variant='body1' component='p'>
+                <Typography variant='body1' component='p' sx={{
+                    fontSize: "2rem",
+                }}>
                     Our privacy policy explains how we collect your personal data, how we use your personal data, and how it is stored.
                 </Typography>
                 </Box>
@@ -29,19 +31,28 @@ export default function Privacy () {
                 {/* Information section */}
                 <Box>
                 <Typography variant="h3" component='h2' sx={{
-                    fontSize: "3rem"
+                    fontSize: "3rem",
+                    fontWeight:  "medium"
                 }}>What personal data do you collect from me?</Typography>
-                <Typography variant='body1' component='p'>We collect etc.</Typography>
+                <Typography variant='body1' component='p' sx={{
+                    fontSize: "2rem"
+                }}>We collect etc.</Typography>
 
                 <Typography variant="h3" component='h2' sx={{
-                    fontSize: "3rem"
+                    fontSize: "3rem",
+                    fontWeight: "medium"
                 }}>How do you use my personal data?</Typography>
-                <Typography variant='body1' component='p'>We collect etc.</Typography>
+                <Typography variant='body1' component='p' sx={{
+                    fontSize: "2rem"
+                }}>We collect etc.</Typography>
 
                 <Typography variant="h3" component='h2'sx={{
-                    fontSize: "3rem"
+                    fontSize: "3rem",
+                    fontWeight:  "medium"
                 }}>How is my personal data stored?</Typography>
-                <Typography variant='body1' component='p'>We collect etc.</Typography>
+                <Typography variant='body1' component='p' sx={{
+                    fontSize: "2rem"
+                }}>We collect etc.</Typography>
                 </Box>
             </Container>
         
