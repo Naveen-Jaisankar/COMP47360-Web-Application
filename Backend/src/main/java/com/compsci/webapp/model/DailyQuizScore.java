6f7cd28f704c43492c934e5b52b4947c07c53e35
RuthDibenzi <dibenziruth1@gmail.com>
2024-07-01 11:50:38 +0100

    sytanx errors

---
+++ Backend/src/main/java/com/compsci/webapp/model/DailyQuizScore.java
@@ -0,0 +1,30 @@
+package com.compsci.webapp.model;
+
+import java.time.LocalDate;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "dailyquizscore")
+public class DailyQuizScore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "quiz_date")
+    private LocalDate quizDate;
+
+    @Column(name = "quiz_score")
+    private Integer quizScore;
+
+   
+}
