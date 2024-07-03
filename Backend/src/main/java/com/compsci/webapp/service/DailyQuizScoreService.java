7e3b2f19fdaadae567743e608889bf2e13ff5960
RuthDibenzi <dibenziruth1@gmail.com>
2024-07-03 15:14:31 +0100

    service class daily quiz

--- Backend/src/main/java/com/compsci/webapp/service/DailyQuizScoreService.java
+++ Backend/src/main/java/com/compsci/webapp/service/DailyQuizScoreService.java
@@ -1,7 +1,7 @@
 package com.compsci.webapp.service;
 
 import com.compsci.webapp.model.DailyQuizScore;
-//import com.compsci.webapp.repository.DailyQuizScoreRepository;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
