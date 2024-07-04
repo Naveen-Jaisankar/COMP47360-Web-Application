fdfc33298395af0a09619cd9ba786ffc9c410c5a
Naveen <naveen.jaisankar1999@gmail.com>
2024-07-04 16:36:19 +0100

    Added endpoints and its implementation for map to get aqi values

---
+++ Backend/src/main/java/com/compsci/webapp/service/MapService.java
@@ -0,0 +1,25 @@
+package com.compsci.webapp.service;
+
+import java.util.List;
+
+import org.json.JSONArray;
+
+import com.compsci.webapp.request.SingleGridRequest;
+import com.compsci.webapp.response.AllGridResponse;
+import com.compsci.webapp.response.SingleGridResponse;
+
+/**
+ * Module Name: MapService.java
+ * Date of Creation: 04-Jul-2024
+ * Author: navee
+ *
+ * Description:
+ * This class handles ...
+ */
+
+
+public interface MapService {
+	
+	List<AllGridResponse> predictForAllGrids(SingleGridRequest input);
+	SingleGridResponse predictForASingleGrid(SingleGridRequest input);
+}
