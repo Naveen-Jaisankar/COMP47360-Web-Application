fadce7b2114ddc78e348bc5863c2a3d9fc6e3e1c
Naveen <naveen.jaisankar1999@gmail.com>
2024-07-12 16:30:51 +0100

    added post request for dailyformquiz

--- FrontEnd/air-quality-app/src/pages/Form.jsx
+++ FrontEnd/air-quality-app/src/pages/Form.jsx
@@ -8,10 +8,14 @@ const Form = () => {
 
   const handleSubmit = () => {
     const data = {
-      userId: userId,
-      quizDate: quizDate,
-      score: score
-    };
+      userId: 1,
+      quizDate: "2024-07-12T00:00:00Z",
+      quizScore: 85,
+      indoorLocation: "40.759109095634464, -73.98741133008829",
+      outdoorLocation: "40.77228005163565, -73.97562338204307",
+      indoorHours: 5,
+      outdoorHours: 3
+  };
 
     axiosInstance.post('/api/dailyquizscores', data)
       .then(response => {
