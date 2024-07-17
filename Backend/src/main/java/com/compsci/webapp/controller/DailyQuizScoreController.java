17ebaa7a0b59b6b363afb4088700239be5523756
RuthDibenzi <dibenziruth1@gmail.com>
2024-07-17 19:17:47 +0100

    updated the coding standard changes in controller class, solved merge conflict

--- Backend/src/main/java/com/compsci/webapp/controller/DailyQuizScoreController.java
+++ Backend/src/main/java/com/compsci/webapp/controller/DailyQuizScoreController.java
@@ -30,14 +30,8 @@ public class DailyQuizScoreController {
     }
 
     @PostMapping
-<<<<<<< Updated upstream
     public DailyQuizScore createDailyQuizScore(@RequestBody DailyQuizScore dailyQuizScore) {
         return dailyQuizScoreService.createDailyQuizScore(dailyQuizScore); 
-=======
-    public DailyQuizScore DailyQuizScore(@RequestBody DailyQuizScore dailyQuizScore) {
-        System.out.println(dailyQuizScore);
-        return dailyQuizScoreService.createDailyQuizScore(dailyQuizScore); // once user hits submit button - 4 
->>>>>>> Stashed changes
     }
 
     @PutMapping("/{id}/{quizDate}")
