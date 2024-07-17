713f89fbc50607f2eab28f351dceef398f4aefae
RuthDibenzi <dibenziruth1@gmail.com>
2024-07-17 19:18:13 +0100

    Update Java files in Backend for DailyQuizScore feature

--- Backend/src/main/java/com/compsci/webapp/service/DailyQuizScoreService.java
+++ Backend/src/main/java/com/compsci/webapp/service/DailyQuizScoreService.java
@@ -20,12 +20,9 @@ import org.springframework.stereotype.Service;
 import com.compsci.webapp.util.AQICalculator;
 
 
-<<<<<<< Updated upstream
 
 
 import java.time.LocalDate;
-=======
->>>>>>> Stashed changes
 import java.util.List;
 
 @Service
