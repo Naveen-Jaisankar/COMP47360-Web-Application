8648c32ecf5b793bfc95752e795620086267d97a
jmacap <156478354+jmacap@users.noreply.github.com>
2024-07-17 14:37:44 +0100

    Daily Form: Modal implemented for user feedback when submitting

--- FrontEnd/air-quality-app/src/pages/DailyForm.jsx
+++ FrontEnd/air-quality-app/src/pages/DailyForm.jsx
@@ -1,13 +1,14 @@
 import UserContent from "../components/usercontent";
 import UserPlaceholder from "../components/userplaceholder";
-import { Box, Container, Typography, Button } from "@mui/material";
+import { Box, Container, Typography, Button, Modal } from "@mui/material";
 import DailySearchbar from "../components/dailysearchbar";
 import CustomNumberInput from "../components/customnumberinput";
 import { useState, useRef } from "react";
-import { styled } from "@mui/system";
-import {ThickHeadingTypography} from "./Home"
-import constants from './../constant';
-import Sidebar from '../components/usersidebar';
+import { borderRadius, display, styled } from "@mui/system";
+import { ThickHeadingTypography } from "./Home";
+import constants from "./../constant";
+import Sidebar from "../components/usersidebar";
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
 
 const QuestionTypography = styled(Typography)(({ theme }) => ({
   marginBottom: "1rem",
@@ -16,22 +17,39 @@ const QuestionTypography = styled(Typography)(({ theme }) => ({
   color: theme.palette.text.primary,
 }));
 
-const GreyBackgroundBox = styled(Box) ({
+const GreyBackgroundBox = styled(Box)({
   backgroundColor: "#F1F3F2",
   margin: "1rem",
   padding: "2rem",
   borderRadius: "20px",
 });
 
+const style = {
+  display: 'flex',
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 500,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  justifyContent: "center",
+  borderRadius: "20px"
+};
+
 export default function DailyForm() {
   const [indoorLocation, setIndoorLocation] = useState("");
   const [outdoorLocation, setOutdoorLocation] = useState("");
   const [indoorHours, setIndoorHours] = useState(0);
   const [outdoorHours, setOutdoorHours] = useState(0);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const indoorSbarTextRef = useRef();
   const outdoorSbarTextRef = useRef();
-
 
   // Validation functions
 
@@ -101,14 +119,14 @@ export default function DailyForm() {
       console.log(`adjusted indoors, ${adjustedIndoorHours}`);
       console.log(`adjusted outdoors, ${adjustedOutdoorHours}`);
 
-      setIndoorHours(adjustedIndoorHours)
-      setOutdoorHours(adjustedOutdoorHours)
-    } else if (totalHours > 24){
+      setIndoorHours(adjustedIndoorHours);
+      setOutdoorHours(adjustedOutdoorHours);
+    } else if (totalHours > 24) {
       hourCheck = false;
     }
 
     return hourCheck;
-  }
+  };
 
   // Submission function
 
@@ -121,8 +139,8 @@ export default function DailyForm() {
     if (!isValid) {
       alert("Please choose a location in Manhattan");
     } else if (!is24Hours) {
-      alert("24 hours exceeded, number inputs are invalid")
-    } else{
+      alert("24 hours exceeded, number inputs are invalid");
+    } else {
       try {
         console.log(`Indoor Hours: ${indoorHours}`);
         console.log(`Outdoor Hours: ${outdoorHours}`);
@@ -132,21 +150,22 @@ export default function DailyForm() {
         console.log(`Outdoor Location: ${outdoorLocation.address}`);
         console.log(`Outdoor Lat: ${outdoorLocation.lat}`);
         console.log(`Outdoor Lng: ${outdoorLocation.lng}`);
-        alert("Form submitted :D");
+
+        handleOpen()
+        // alert("Form submitted :D");
 
         setIndoorLocation("");
         setOutdoorLocation("");
         setIndoorHours(0);
         setOutdoorHours(0);
 
-        if (indoorSbarTextRef.current){
+        if (indoorSbarTextRef.current) {
           indoorSbarTextRef.current.handleReset();
         }
 
-        if (outdoorSbarTextRef.current){
-          outdoorSbarTextRef.current.handleReset()
+        if (outdoorSbarTextRef.current) {
+          outdoorSbarTextRef.current.handleReset();
         }
-
       } catch (err) {
         console.log(`Error: ${err.message}`);
       }
@@ -155,36 +174,44 @@ export default function DailyForm() {
 
   // Form input functions
 
-    const handleIndoorHoursChange = (event, newValue) => {
-      const indoorValue = newValue;
-      setIndoorHours(indoorValue);
-    };
+  const handleIndoorHoursChange = (event, newValue) => {
+    const indoorValue = newValue;
+    setIndoorHours(indoorValue);
+  };
 
-    const handleOutdoorHoursChange = (event, newValue) => {
-      const outdoorValue = newValue;
-      setOutdoorHours(outdoorValue);
-    };
+  const handleOutdoorHoursChange = (event, newValue) => {
+    const outdoorValue = newValue;
+    setOutdoorHours(outdoorValue);
+  };
 
-    const handleIndoorPlaceChange = (placeData) => {
-      setIndoorLocation(placeData);
-    };
+  const handleIndoorPlaceChange = (placeData) => {
+    setIndoorLocation(placeData);
+  };
 
-    const handleOutdoorPlaceChange = (placeData) => {
-      setOutdoorLocation(placeData);
-    };
+  const handleOutdoorPlaceChange = (placeData) => {
+    setOutdoorLocation(placeData);
+  };
 
-    const [isSidebarOpen, setSidebarOpen] = useState(true);
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
 
-    const toggleDrawer = () => {
-      setSidebarOpen(!isSidebarOpen);
-    };
+  const toggleDrawer = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
 
   return (
     <div className="flex">
       <Sidebar isOpen={isSidebarOpen} toggleDrawer={toggleDrawer} />
-      <UserContent className={`transition-all duration-300 ${isSidebarOpen ? 'ml-60' : 'ml-0'} p-6`}>
-        <Container sx={{marginTop: "2rem",}}>
-          <ThickHeadingTypography variant="h1" component="h1" sx={{color: "black", paddingLeft: "1rem"}}>
+      <UserContent
+        className={`transition-all duration-300 ${
+          isSidebarOpen ? "ml-60" : "ml-0"
+        } p-6`}
+      >
+        <Container sx={{ marginTop: "2rem" }}>
+          <ThickHeadingTypography
+            variant="h1"
+            component="h1"
+            sx={{ color: "black", paddingLeft: "1rem" }}
+          >
             {constants.dailyForm.title}
           </ThickHeadingTypography>
 
@@ -193,9 +220,10 @@ export default function DailyForm() {
               <QuestionTypography variant="h4" component="h2">
                 {constants.dailyForm.q1_indoorLocation}
               </QuestionTypography>
-              <DailySearchbar 
+              <DailySearchbar
                 passPlaceData={handleIndoorPlaceChange}
-                ref ={indoorSbarTextRef} />
+                ref={indoorSbarTextRef}
+              />
 
               <QuestionTypography variant="h4" component="h2">
                 {constants.dailyForm.q2_indoorHours}
@@ -206,19 +234,69 @@ export default function DailyForm() {
                 arialabel={"Number of Hours spent indoors"}
               />
               <QuestionTypography variant="h4" component="h2">
-               {constants.dailyForm.q3_outdoorLocation}
+                {constants.dailyForm.q3_outdoorLocation}
               </QuestionTypography>
-              <DailySearchbar passPlaceData={handleOutdoorPlaceChange} 
-               ref={outdoorSbarTextRef}/>
+              <DailySearchbar
+                passPlaceData={handleOutdoorPlaceChange}
+                ref={outdoorSbarTextRef}
+              />
 
               <QuestionTypography variant="h4" component="h2">
-               {constants.dailyForm.q4_outdoorHours}
+                {constants.dailyForm.q4_outdoorHours}
               </QuestionTypography>
               <CustomNumberInput
                 value={outdoorHours}
                 onChange={handleOutdoorHoursChange}
                 arialabel={"Number of Hours spent indoors"}
               />
+              <div>
+                {/* <Button onClick={handleOpen}>Open modal</Button> */}
+                <Modal
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="modal-modal-title"
+                  aria-describedby="modal-modal-description"
+                >
+                  
+                  <Box sx={style}>
+                    <Box sx={{
+                     display: "flex",
+                     flexDirection: "column",
+                     alignItems: "center",
+                     justifyContent: "center",
+                     height: "100%"
+                    }}>
+                      <TaskAltIcon 
+                    // fontSize= "large"
+                    color = 'success'
+                    sx={{
+                      paddingBottom: "1rem",
+                      fontSize: "10rem"
+                    }}
+                    />
+                    <Typography
+                      id="modal-modal-title"
+                      variant="h4"
+                      component="h2"
+                    >
+                      Form Submitted
+                    </Typography>
+
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                      Thank you!
+                    </Typography>
+
+                    
+                    
+                   
+                   
+                    </Box>
+                    
+                  
+                    
+                  </Box>
+                </Modal>
+              </div>
 
               <Button type="submit">Submit</Button>
             </form>
