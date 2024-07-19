c1e8d0ebb8955e98723d59fc2747c2ac702df2f5
RuthDibenzi <dibenziruth1@gmail.com>
2024-07-19 12:14:31 +0100

    modifiedaxios request to allow data reponse and sending

--- FrontEnd/air-quality-app/src/axios.jsx
+++ FrontEnd/air-quality-app/src/axios.jsx
@@ -1,13 +1,18 @@
+// import axios from 'axios';
+
+// const axiosInstance = axios.create({
+//   baseURL: 'http://localhost:8080/api/v1',
+//   headers: {
+//     'Accept': '*/*',
+//     'Content-Type': 'application/json',
+//     'Access-Control-Allow-Origin': '*',
+//   }
+// });
+
+// export default axiosInstance;
+
 import axios from 'axios';
 
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
-  headers: {
-    'Accept': '*/*',
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-  }
+export default axios.create({
+    baseURL: 'http://localhost:8080'
 });
-
-export default axiosInstance;
-
