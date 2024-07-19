1b52beace846bbc6f3255dfd64e50fb64bcb45bf
RuthDibenzi <dibenziruth1@gmail.com>
2024-07-19 12:50:49 +0100

    updated request produces 200 response previous produced 403

--- FrontEnd/air-quality-app/src/axios.jsx
+++ FrontEnd/air-quality-app/src/axios.jsx
@@ -1,15 +1,4 @@
-// import axios from 'axios';
 
-// const axiosInstance = axios.create({
-//   baseURL: 'http://localhost:8080/api/v1',
-//   headers: {
-//     'Accept': '*/*',
-//     'Content-Type': 'application/json',
-//     'Access-Control-Allow-Origin': '*',
-//   }
-// });
-
-// export default axiosInstance;
 
 import axios from 'axios';
 
