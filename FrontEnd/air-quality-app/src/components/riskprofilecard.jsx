3f10374267d215810535363630b227e53a00d529
Naveen <naveen.jaisankar1999@gmail.com>
2024-07-27 13:46:18 +0100

    changed image path in riskprofile card

--- FrontEnd/air-quality-app/src/components/riskprofilecard.jsx
+++ FrontEnd/air-quality-app/src/components/riskprofilecard.jsx
@@ -4,8 +4,8 @@ import { Typography } from "@mui/material";
 import PropTypes from 'prop-types';
 
 const RiskProfileCard = ({avgAQI, userAQI, specialCase}) => {
-    let starImage = "../src/static/star.png"
-    let airImage ="../src/static/air-icon.png" 
+    let starImage = "static/star.png"
+    let airImage ="static/air-icon.png" 
     let grade = "";
     let headingText ="";
     let contentText = "";
